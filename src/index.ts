babf4c6318396ccf818dcfcd9ed9170925b3d4d8
MelvDouc <melv.douc@gmail.com>
2024-08-25 04:45:25 +0200

    Add class exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export {
+import {
   createTable,
   deleteFrom,
   insertInto,
@@ -6,3 +6,22 @@ export {
   selectFrom,
   update
 } from "$/functions.js";
+import type CreateTableStatementBuilder from "$/statements/create-table/CreateTableStatementBuilder.js";
+import type InsertStatementBuilder from "$/statements/InsertStatementBuilder.js";
+import type DeleteStatementBuilder from "$/statements/limitable/DeleteStatementBuilder.js";
+import type SelectStatementBuilder from "$/statements/limitable/SelectStatementBuilder.js";
+import type UpdateStatementBuilder from "$/statements/limitable/UpdateStatementBuilder.js";
+
+export {
+  createTable,
+  deleteFrom,
+  insertInto,
+  jsonObject,
+  selectFrom,
+  update,
+  type CreateTableStatementBuilder,
+  type DeleteStatementBuilder,
+  type InsertStatementBuilder,
+  type SelectStatementBuilder,
+  type UpdateStatementBuilder
+};
